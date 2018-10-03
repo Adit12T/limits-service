d7a98d3f327f9c2e39c6c038f9df6e308ecf4b52
Aditya12T <adityatripathi12@gmail.com>
2018-10-03 15:49:06 -0400

    Config

--- src/main/java/com/example/micro/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/example/micro/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.example.micro.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,13 @@ import com.example.micro.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public LimitConfiguration retrievelLimitsFromConfigurations() {
 	
-		return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum(),
+				configuration.getMinimum());
 	}
 }
